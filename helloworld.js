a057e4ec80db98798328f5801b7c0f9e764e0515
yasusu_ 500 <eriyasu@gmail.com>
2023-03-20 18:00:07 +0900

    [Update]helloworld.ts,helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,2 +1,10 @@
+// varは古くから変数宣言で用意られていたキーワード、意図しない動作を生み出すことが懸念されている
 var message = 'Hello world';
 console.log(message);
+var greeting = 'こんにちは、javascript';
+console.log(greeting);
+var content = "プログラミング、学習中";
+console.log(content);
+// 再代入可能
+content = "javascript、学習中";
+console.log(content);
--- helloworld.ts
+++ helloworld.ts
@@ -1,2 +1,13 @@
-let message: string ='Hello world';
+// varは古くから変数宣言で用意られていたキーワード、意図しない動作を生み出すことが懸念されている
+var message: string ='Hello world';
 console.log(message);
+
+const greeting = 'こんにちは、javascript';
+console.log(greeting);
+
+let content = "プログラミング、学習中";
+console.log(content);
+
+// 再代入可能
+content = "javascript、学習中";
+console.log(content);
